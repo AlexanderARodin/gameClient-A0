8b340908633e328ca75d520368db2e681861b39e
AlexanderARodin <rodin_aa@inbox.ru>
2022-02-04 02:02:32 +0300

    Initial Commit

---
+++ gameClient-A0/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gameClient-A0
+//
+//  Created by the Dragon on 04.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ gameClient-A0/gameClient_A0App.swift
@@ -0,0 +1,17 @@
+//
+//  gameClient_A0App.swift
+//  gameClient-A0
+//
+//  Created by the Dragon on 04.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct gameClient_A0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
